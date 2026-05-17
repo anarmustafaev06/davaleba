df6f1fdb47ddacd0a473407e831c9a7c91a275c7
Anar Mustafaev <anar.mustafayev01@gmail.com>
2026-05-17 23:40:12 +0400

    pirveli versia

---
+++ js.js
@@ -0,0 +1,15 @@
+const user = {
+  name: "Anar",
+  surname: "Mustafayev",
+  age: 19,
+  email: "anar.mustafayev@gmail.com",
+  password: "123456",
+  phone: "66-666-666"
+};
+
+console.log("Name is :", user.name);
+console.log("Surname is :", user.surname);
+console.log("Age :", user.age);
+console.log("Email :", user.email);
+console.log("Password :", user.password);
+console.log("Phone :", user.phone);
